537d0e7e4b3956af7a50c09fafc3092708783bb3
Damian Koper <kopernickk@gmail.com>
2020-05-31 15:10:52 +0200

    Precision tricks WIP

--- engine/src/core/domain/Camera/controllers/TrackballController.ts
+++ engine/src/core/domain/Camera/controllers/TrackballController.ts
@@ -6,7 +6,7 @@ import Hammer from "hammerjs";
 
 /* TODO implements CameraController */
 export default class TrackballController {
-  private globalOrbit = new Vector3(0, 0.000001, 0);
+  private globalOrbit = new Vector3(0, 6371, 0);
   private localOrbit = new Vector3(1, 1, 0).multiplyScalar(100);
 
   private mc: HammerManager;
@@ -39,7 +39,7 @@ export default class TrackballController {
   update() {
     this.panMotionQuaternion.slerp(new Quaternion(), 0.05);
     this.globalOrbit.applyQuaternion(this.panMotionQuaternion);
-    this.localOrbit.applyEuler(new Euler(0, 0.01, 0, "YXZ"));
+    //this.localOrbit.applyEuler(new Euler(0, 0.01, 0, "YXZ"));
 
     const globalNormalized = this.globalOrbit.clone().normalize();
     const rotation = new Quaternion().setFromUnitVectors(
@@ -47,20 +47,14 @@ export default class TrackballController {
       globalNormalized
     );
 
-    
-    const camPosition = this.localOrbit.clone().applyQuaternion(rotation);
-
-    this.scene.position.set(
-      -this.globalOrbit.x,
-      -this.globalOrbit.y,
-      -this.globalOrbit.z
-    );
+    const camPosition = this.localOrbit
+      .clone()
+      .applyQuaternion(rotation)
+      .add(this.globalOrbit);
 
     this.camera.position.copy(camPosition);
-    this.camera.lookAt(0, 0, 0);
+    this.camera.lookAt(this.globalOrbit);
     this.camera.up.copy(this.globalOrbit);
-    console.log("global", this.globalOrbit.x,this.globalOrbit.y,this.globalOrbit.z);
-    console.log("local",  this.localOrbit.x,this.localOrbit.y,this.localOrbit.z);
   }
 
   private setEvents() {
@@ -75,42 +69,11 @@ export default class TrackballController {
 
     this.mc.on("panmove", (ev) => {
       const allDelta = new Vector2(ev.deltaX, ev.deltaY);
-      const currentDelta = allDelta.clone().sub(this.panDelta);
-
-      const horizontalAxis = new Vector3().crossVectors(
-        this.camera.position,
-        this.globalOrbit
-      );
-      const verticalAxis = new Vector3().crossVectors(
-        this.camera.position,
-        horizontalAxis
-      );
-
-      const localOrbitRadius = this.localOrbit.length();
-
-      const slowFactor = 0.000001;
-      const qVertical = new Quaternion().setFromAxisAngle(
-        horizontalAxis.clone().normalize(),
-        currentDelta.y * localOrbitRadius * slowFactor
-      );
-      const qHorizontal = new Quaternion().setFromAxisAngle(
-        verticalAxis.clone().normalize(),
-        currentDelta.x * localOrbitRadius * slowFactor
-      );
-
-      const panMotionQuaternion = new Quaternion().multiplyQuaternions(
-        qVertical,
-        qHorizontal
-      );
-
-      if (currentDelta.manhattanLength() > 2) {
-        this.panMotionQuaternion = panMotionQuaternion;
+      if (ev.srcEvent.shiftKey) {
+        this.handleLocalOrbitRotate(allDelta);
       } else {
-        this.panMotionQuaternion = new Quaternion();
-        this.globalOrbit.applyQuaternion(panMotionQuaternion);
+        this.handleGlobalOrbitRotate(allDelta);
       }
-
-      this.panDelta.copy(allDelta);
     });
 
     this.eventSource.addEventListener("mousedown", () => {
@@ -124,6 +87,50 @@ export default class TrackballController {
         this.localOrbit.multiplyScalar(1.222222222223);
       }
     });
+  }
+  handleLocalOrbitRotate(allDelta: THREE.Vector2) {
+    // TODO: Make it work
+    const qVertical = new Quaternion().setFromAxisAngle(
+      horizontalAxis.clone().normalize(),
+      currentDelta.y * localOrbitRadius * slowFactor
+    );
+    const qHorizontal = new Quaternion().setFromAxisAngle(
+      verticalAxis.clone().normalize(),
+      currentDelta.x * localOrbitRadius * slowFactor
+    );
+  }
+
+  private handleGlobalOrbitRotate(allDelta: THREE.Vector2) {
+    const currentDelta = allDelta.clone().sub(this.panDelta);
+    const horizontalAxis = new Vector3().crossVectors(
+      this.camera.position,
+      this.globalOrbit
+    );
+    const verticalAxis = new Vector3().crossVectors(
+      this.camera.position,
+      horizontalAxis
+    );
+    const localOrbitRadius = this.localOrbit.length();
+    const slowFactor = 0.0000006;
+    const qVertical = new Quaternion().setFromAxisAngle(
+      horizontalAxis.clone().normalize(),
+      currentDelta.y * localOrbitRadius * slowFactor
+    );
+    const qHorizontal = new Quaternion().setFromAxisAngle(
+      verticalAxis.clone().normalize(),
+      currentDelta.x * localOrbitRadius * slowFactor
+    );
+    const panMotionQuaternion = new Quaternion().multiplyQuaternions(
+      qVertical,
+      qHorizontal
+    );
+    if (currentDelta.manhattanLength() > 2) {
+      this.panMotionQuaternion = panMotionQuaternion;
+    } else {
+      this.panMotionQuaternion = new Quaternion();
+      this.globalOrbit.applyQuaternion(panMotionQuaternion);
+    }
+    this.panDelta.copy(allDelta);
   }
 
   private stopMovement() {
--- engine/src/core/domain/Visualization/examples/SphereVis.ts
+++ engine/src/core/domain/Visualization/examples/SphereVis.ts
@@ -1,6 +1,6 @@
 import Visualization from "../models/Visualization";
 import * as THREE from "three";
-import { Matrix4 } from "three";
+import { Matrix4, Quaternion, Vector3 } from "three";
 //import TrackballController from "../../Camera/controllers/TrackballController";
 export default class SphereVis extends Visualization {
   private scene?: THREE.Scene;
@@ -12,25 +12,20 @@ export default class SphereVis extends Visualization {
     this.scene = scene;
     console.info("setup scene");
     const r = 6371;
+    const scale = 32
+    // TODO: LOD with scale based on distance -> lowet distance = bigger scale
 
-    const sphere = new THREE.SphereBufferGeometry(
-      r,
-      100,
-      100,
-      0,
-      Math.PI * 2,
-      0,
-      Math.PI / 16
-    );
+    const sphere = new THREE.SphereBufferGeometry(r/scale, 100, 100);
     const sphereMaterial = new THREE.MeshBasicMaterial({ color: 0xbada55 });
     const sphereMesh = new THREE.Mesh(sphere, sphereMaterial);
-    sphereMesh.position.setY(-r);
+    const material = new THREE.LineBasicMaterial({ color: 0xffffff });
+    const wireframe = new THREE.LineSegments(sphereMesh.geometry, material);
+    
+    //TODO: Adjust position when updated
+    sphereMesh.position.setY(r/scale*(scale-1))
     scene.add(sphereMesh);
-
-    const material = new THREE.LineBasicMaterial( { color: 0xffffff } );
-    const wireframe = new THREE.LineSegments( sphereMesh.geometry, material );
-    wireframe.position.setY(-r);
-    scene.add(wireframe)
+    sphereMesh.position.setY(r/scale*(scale-1))
+    scene.add(wireframe);
 
     const testBoxes = [
       new THREE.BoxBufferGeometry(0.001, 0.001, 0.001),
@@ -39,18 +34,18 @@ export default class SphereVis extends Visualization {
       new THREE.BoxBufferGeometry(1, 1, 1),
       new THREE.BoxBufferGeometry(10, 10, 10),
       new THREE.BoxBufferGeometry(100, 100, 100),
-      //new THREE.BoxBufferGeometry(1000, 1000, 1000),
     ];
+
+    const boxGroup = new THREE.Group();
+    boxGroup.applyMatrix4(new Matrix4().makeTranslation(0, r, 0));
 
     const boxMaterial = new THREE.MeshBasicMaterial({ color: 0x5500ff });
     testBoxes.forEach((box, i) => {
-      box.applyMatrix4(
-        new Matrix4().makeTranslation(0, 0, -(10 ** (i - 3)))
-      );
+      box.applyMatrix4(new Matrix4().makeTranslation(0, 0, -(10 ** (i - 3))));
       const boxMesh = new THREE.Mesh(box, boxMaterial);
-      
-      scene.add(boxMesh);
+      boxGroup.add(boxMesh);
     });
+    scene.add(boxGroup);
 
     /*     const axesHelper = new THREE.AxesHelper(2 * r);
     scene.add(axesHelper);
